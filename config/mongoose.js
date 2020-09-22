ac8444187bbc75c88e0bbbbc8bd9243eb4e15523
ashurbz <rbzashu1@gmail.com>
2020-09-22 11:02:05 +0530

    added comments, completed directory

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/codeial_development');
+const mongoose = require('mongoose');  // requiring mongoose
+mongoose.connect('mongodb://localhost/codeial_development');   // establishing connection
 
-const db = mongoose.connection;
+const db = mongoose.connection;   // if connection succesfull
 
-db.on('error',console.error.bind('console',"error in connecting to Db"));
+db.on('error',console.error.bind('console',"error in connecting to Db"));  // if there is error in connecting
 
 db.once('open',()=>{
     console.log('Database Successfuly Connected');
 })
 
-module.exports =db;
+module.exports =db;   // exporting module
+ 
