ec339bc4f7258952e9c302c2ee0ccc9fd060b699
ashurbz <rbzashu1@gmail.com>
2020-10-12 10:10:58 +0530

    post deletion added

--- controllers/post_Controller.js
+++ controllers/post_Controller.js
@@ -1,4 +1,6 @@
-const Post = require('../models/post');         // require post models
+const Post = require('../models/post');        // require post models
+const Comment = require('../models/comment')
+
 
 module.exports.create = (req,res)=>{
     Post.create({                               // creating posts                            
@@ -9,5 +11,23 @@ module.exports.create = (req,res)=>{
             console.log(`error in creating post`); return;
         }
         return res.redirect('back');
+    })
+}
+
+module.exports.destroy =(req,res)=>{     // deleting post
+    Post.findById(req.params.id,(err,post)=>{
+        if(err){console.log(`error in finding the post`); return;}
+
+            if(post.user== req.user.id){            // if post found then watching if post is created by same user who is logged in
+                post.remove();                      // removing post
+
+                Comment.deleteMany({post:req.params.id},(err)=>{               // deleting comments associated with that post
+                    return res.redirect('back')
+                })
+
+            }else{
+             return   res.redirect('back')
+            }
+        
     })
 }
